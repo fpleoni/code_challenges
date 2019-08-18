67bc7ebc4e59057e3ce447f0fdde87aed8d9a98f
Florencia Leoni <florenleoni@gmail.com>
2019-08-18 12:46:53 -0400

    basic object-oriented analysis and design through building a simple game engine

---
+++ ex43.py
@@ -0,0 +1,56 @@
+# Gothons from Planet Percal #25
+
+# Basic Object-Oriented Analysis and Design
+
+class Scene(object):
+
+    def enter(self):
+        pass
+
+class Engine(object):
+
+    def __init__(self, scene_map):
+        pass
+    
+    def play(self):
+        pass
+
+class Death(Scene):
+
+    def enter(self):
+        pass
+
+class CentralCorridor(Scene):
+
+    def enter(self):
+        pass
+
+class LaserWeaponArmory(Scene):
+
+    def enter(self):
+        pass
+
+class TheBridge(Scene):
+
+    def enter(self):
+        pass
+
+class EscapePod(Scene):
+
+    def enter(self):
+        pass
+
+class Map(object):
+
+    def __init__(self, start_scene):
+        pass
+    
+    def next_scene(self, scene_name):
+        pass
+
+    def opening_scene(self):
+        pass
+
+a_map = Map("central_corridor")
+a_game = Engine(Map)
+a_game.play()
