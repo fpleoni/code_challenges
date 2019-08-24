dc4f44f8c504cff1c87cc397c1e93ab200557903
Florencia Leoni <florenleoni@gmail.com>
2019-08-24 10:37:52 -0400

    OOP version of Shakespeare's Romeo & Juliet. The entities of the problem

---
+++ romeo_and_juliet.py
@@ -0,0 +1,34 @@
+from sys import exit
+from textwrap import dedent
+
+class Scene(object):
+    pass
+
+class Person(object):
+    pass
+
+class Director(object):
+    pass
+
+class Map(object):
+    pass
+
+class TheMaskedBall(Scene):
+    pass
+
+class TheBalcony(Scene):
+    pass
+
+class TheDuel(Scene):
+    pass
+
+class TheArrangement(Scene):
+    pass
+
+class TheApothecary(Scene):
+    pass
+
+class TheCapuletTomb(Scene):
+    pass
+
+
