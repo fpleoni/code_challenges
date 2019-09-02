6dcda365691b9e6bb395bde31ceb5097040d7e5a
Florencia Leoni <florenleoni@gmail.com>
2019-09-02 16:31:32 -0400

    created variables holding information for the problem

--- lp_example_problem.py
+++ lp_example_problem.py
@@ -3,5 +3,15 @@ from pulp import *
 # Start with creating a problem instance
 lp_prob = LpProblem("transportationProblem", LpMinimize)
 
+# Supply
+w1 = 8
+w2 = 6
+w3 = 3
+
+# Demand
+s1 = 4
+s2 = 2
+s3 = 3
+
 
 
