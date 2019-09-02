42fd3a47dae601631f01e0c690af2eea69f8c565
Florencia Leoni <florenleoni@gmail.com>
2019-09-02 17:29:35 -0400

    added decision variables instance as a dictionary

--- lp_example_problem.py
+++ lp_example_problem.py
@@ -1,17 +1,20 @@
-from pulp import *
+import pulp as plp
 
 # Start with creating a problem instance
-lp_prob = LpProblem("transportationProblem", LpMinimize)
+lp_prob = plp.LpProblem(name = "transportationProblem", plp.LpMinimize)
 
 # Supply
 w1 = 8
 w2 = 6
 w3 = 3
+total_supply = w1 + w2 + w3
 
 # Demand
 s1 = 4
 s2 = 2
 s3 = 3
+dummy_demand = 8
+total_demand = s1 + s2 + s3
 
 # Delivery rates from shops to warehouses in USD
 # Since there is an 8 unit surplus in supply, 
@@ -31,4 +34,9 @@ s1_w3 = 2
 s2_w3 = 1
 s3_w3 = 5
 sd_w3 = 0
+
+# Decision variables
+# We want the number of items transported from the i-th source
+# To the j-th destionation
+item_quantity = plp.LpVariable.dicts("item_quatity", list(range(1, 13)), lowBound = 0, cat = "Integer")
 
